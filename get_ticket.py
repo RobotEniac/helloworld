66eff0f70c7c354260bb8c1293dc016f665eb3e0
roboteniac <gethaibo@gmail.com>
2016-10-19 21:51:03 +0800

    add ticket func

--- get_ticket.py
+++ get_ticket.py
@@ -24,7 +24,7 @@ def getCookie(path):
         cookie = f.readline().strip()
     return cookie
 
-def ticketCheck(ticket_id, seat_type):
+def getHeaders(ticket_id, seat_type):
     header_ref = 'http://shop.48.cn/tickets/item/' + str(ticket_id) + '?seat_type=' + str(seat_type)
     headers = {
             'Connection': 'keep-alive',
@@ -36,8 +36,13 @@ def ticketCheck(ticket_id, seat_type):
             'Referer': header_ref,
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2',
-            'Cookie': getCookie("./haibo.cookie")
+            'Cookie': getCookie("./cookie/haibo.cookie")
             }
+    return headers
+
+
+def ticketCheck(ticket_id, seat_type, headers):
+    headers = getHeaders(ticket_id, seat_type)
     params_dict = {'id': ticket_id, 'r':0.5651687378367096}
     params = urllib.urlencode(params_dict)
     url = '/TOrder/tickCheck'
@@ -47,21 +52,14 @@ def ticketCheck(ticket_id, seat_type):
     print resp.status, resp.reason
     return resp
 
-def getCheck(ticket_id, seat_type):
-    header_ref = 'http://shop.48.cn/tickets/item/' + str(ticket_id) + '?seat_type=' + str(seat_type)
-    headers = {
-            'Connection': 'keep-alive',
-            'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'Origin': 'http://shop.48.cn',
-            'X-Requested-With': 'XMLHttpRequest',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Referer': header_ref,
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2',
-            'Cookie': getCookie("./haibo.cookie")
+def addTicket(ticket_id, seat_type, headers):
+    params_dict = {
+            'id': ticket_id,
+            'r': 0.5651687378367096,
+            'num': 1,
+            'seattype': seat_type,
+            'brand_id': 1 # 1: snh48, 2: bej48, 3:gnz48
             }
-    params_dict = {'id': ticket_id, 'r':0.5651687378367096}
     params = urllib.urlencode(params_dict)
     url = '/TOrder/add'
     conn = httplib.HTTPConnection('shop.48.cn')
@@ -70,10 +68,13 @@ def getCheck(ticket_id, seat_type):
     print resp.status, resp.reason
     return resp
 
-
 if __name__ == '__main__':
-    for i in range(10):
-        res = json.load(ticketCheck(464, 2))
-        print res[u'Message']
-        time.sleep(5)
+    ticket_id = 464
+    seat_type = 2
+    headers = getHeaders(ticket_id, seat_type)
+    res = json.load(ticketCheck(ticket_id, seat_type, headers))
+    has_err = res[u'HasError']
+    err_code = res[u'ErrorCode']
+    msg = res[u'Message']
+    print has_err, "x" + err_code, msg
 
