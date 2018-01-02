38aa4ded4080b704ba50007426ab19979624cc29
roboteniac <gethaibo@gmail.com>
2018-01-02 21:07:57 +0800

    fix exception bug

--- get_ticket.py
+++ get_ticket.py
@@ -11,16 +11,20 @@ from datetime import date
 from datetime import timedelta
 
 def getServerTime(connection):
-    client_time1 = time.time()
-    connection.request("GET", "/pai/GetTime")
-    client_time2 = time.time()
-    res = connection.getresponse()
-    if res.status == 200:
-        data = res.read()
-        server_time_str = int(re.search('\d+', data).group(0))
-        server_time = float(server_time_str) / 1000.0
-        return (client_time1, server_time, client_time2)
-    return None
+    try:
+        client_time1 = time.time()
+        connection.request("GET", "/pai/GetTime")
+        client_time2 = time.time()
+        res = connection.getresponse()
+        if res.status == 200:
+            data = res.read()
+            server_time_str = int(re.search('\d+', data).group(0))
+            server_time = float(server_time_str) / 1000.0
+            return (client_time1, server_time, client_time2)
+        return (None, None, None)
+    except:
+        print "getServertime exception"
+        return (None, None, None)
 
 def getCookie(path):
     cookie = ''
@@ -35,11 +39,12 @@ def getHeaders(ticket_id, seat_type):
             'Accept': 'application/json, text/javascript, */*; q=0.01',
             'Origin': 'http://shop.48.cn',
             'X-Requested-With': 'XMLHttpRequest',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'Referer': header_ref,
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2',
+            'Upgrade-Insecure-Requests': '1',
             'Cookie': getCookie("./cookie/haibo.cookie")
             }
     return headers
@@ -50,11 +55,14 @@ def ticketCheck(connection, ticket_id, seat_type, headers):
     params_dict = {'id': ticket_id, 'r':0.5651687378367096}
     params = urllib.urlencode(params_dict)
     url = '/TOrder/tickCheck'
-    connection = httplib.HTTPSConnection('shop.48.cn', 443)
-    connection.request('GET', url, params, headers)
-    resp = connection.getresponse()
-    print resp.status, resp.reason
-    return resp
+    try:
+        connection.request('GET', url, params, headers)
+        resp = connection.getresponse()
+        print resp.status, resp.reason
+        return resp
+    except:
+        print "ticketCheck exception"
+        return None
 
 def addTicket(connection, ticket_id, seat_type, headers):
     params_dict = {
@@ -62,15 +70,19 @@ def addTicket(connection, ticket_id, seat_type, headers):
             'r': 0.5651687378367096,
             'num': 1,
             'seattype': seat_type,
-            'brand_id': 1 # 1: snh48, 2: bej48, 3:gnz48
+            'brand_id': 3, # 1: snh48, 2: bej48, 3:gnz48
+            'choose_times_end': -1,
             }
     params = urllib.urlencode(params_dict)
     url = '/TOrder/add'
-    connection = httplib.HTTPSConnection('shop.48.cn', 443)
-    connection.request('POST', url, params, headers)
-    resp = connection.getresponse()
-    print resp.status, resp.reason
-    return resp
+    try:
+        connection.request('POST', url, params, headers)
+        resp = connection.getresponse()
+        print resp.status, resp.reason
+        return resp
+    except:
+        print "addTicket exception"
+        return None
 
 def ticktack(connection, hh, MM = 0, ss = 0):
     if hh == 0:
@@ -112,7 +124,7 @@ def ticktack(connection, hh, MM = 0, ss = 0):
         if i > 10000000:
             i = 0
 
-    # time.sleep(0.008) # magic number, avoid estimate time > server time
+    time.sleep(0.008) # magic number, avoid estimate time > server time
     t1, t2, t3 = getServerTime(connection)
     # print "%.6f, %.6f, %.6f" % (t1, dlt, rtt)
     print max_dlt, avg_rtt
@@ -125,19 +137,28 @@ def ticktack(connection, hh, MM = 0, ss = 0):
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
-        print "Usage:", sys.argv[0], "ticket_id seat_type count."
+        print "Usage:", sys.argv[0], "ticket_id seat_type count [backup_seat_type]."
         exit(1)
     conn = httplib.HTTPSConnection('shop.48.cn', 443)
     ticktack(conn, 20)
-    #ticktack(conn, 0)
     ticket_id = int(sys.argv[1])
     seat_type = int(sys.argv[2])
     count = int(sys.argv[3])
+    backup_seat_type = -1
+    if len(sys.argv) >= 5:
+        backup_seat_type = int(sys.argv[4])
+
+    seat_map = {1:"svip", 2:"vip", 3:"seat", 4:"stand"}
     for i in range(0, count):
+        if backup_seat_type > 0:
+            if i >= 3:
+                seat_type = backup_seat_type
+        print "seat_type is %s" % (seat_map[seat_type])
         headers = getHeaders(ticket_id, seat_type)
         res_str = addTicket(conn, ticket_id, seat_type, headers)
-        res = json.load(res_str, 'utf8')
-        print json.dumps(res, ensure_ascii=False, sort_keys=True, indent=4)
+        if res_str and res_str.status == 200:
+            res = json.load(res_str, 'utf8')
+            print json.dumps(res, ensure_ascii=False, sort_keys=True, indent=4)
         start_time = time.time()
         last_req = start_time - 2.5
         while True:
@@ -149,6 +170,6 @@ if __name__ == '__main__':
                 continue
             last_req = end_time
             res = ticketCheck(conn, ticket_id, seat_type, headers)
-            if res.status == 200:
+            if res and res.status == 200:
                 res_json = json.load(res)
                 print json.dumps(res_json, ensure_ascii=False, sort_keys=True, indent=4)
